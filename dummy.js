a81b8e278e5bb999d190997d72cf3ded265cdaf6
JorgeRamCas <jorgeramirezcastanon@yahoo.com.mx>
2021-04-02 18:25:53 -0600

    Add files via upload

---
+++ dummy.js
@@ -0,0 +1,24 @@
+const usuarios = [
+    {
+        id:0,
+        nombre: 'Fulanito',
+        clase: 'aaammmmffffC'
+    },
+    {
+        id:20,
+        nombre: 'Alex',
+        clave: 'zpfg'
+    },
+    {
+        id: 2,
+        nombre: 'Erendira',
+        clave: 'lllld'
+    },
+    {
+        id: 1,
+        nombre: 'Ulises',
+        clave: 'abc'
+    }
+];
+
+module.exports = usuarios;
